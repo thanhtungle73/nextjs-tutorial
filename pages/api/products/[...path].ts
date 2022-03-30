8fa2904c3bec0d0c6af6afccf8db8c36ddd9c856
Tung Le <thanhtungle73@gmail.com>
2022-03-31 01:36:30 +0700

    Add api

--- pages/api/products/[...path].ts
+++ pages/api/products/[...path].ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Learn NextJs' })
+  res.status(200).json({ name: 'Catch all paths of products' })
 }
--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Learn NextJs' })
+  res.status(200).json({ name: 'Get Products Details' })
 }
