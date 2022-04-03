9ad92d11d2d0f651aa922a28c857cdc3a9710a62
Tung Le <thanhtungle73@gmail.com>
2022-04-04 01:43:04 +0700

    Add axios client

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,39 @@
+import { authApi } from '@/api/auth-api';
 import * as React from 'react';
 
-export interface ILoginPageProps {}
-
-export default function LoginPage(props: ILoginPageProps) {
-  async function handleLoginClick() {}
-  async function handleGetProfileClick() {}
-  async function handleLogoutClick() {}
+export default function LoginPage() {
+  async function handleLoginClick() {
+    try {
+      await authApi.login({
+        username: 'test1',
+        password: '123123',
+      });
+    } catch (error) {
+      console.log('Failed to login: ', error);
+    }
+  }
+  async function handleGetProfileClick() {
+    try {
+      await authApi.getProfile();
+    } catch (error) {
+      console.log('Failed to get profile: ', error);
+    }
+  }
+  async function handleLogoutClick() {
+    try {
+      await authApi.logout();
+    } catch (error) {
+      console.log('Failed to logout: ', error);
+    }
+  }
 
   return (
     <div>
       <h1>Login Page</h1>
 
-      <button>Login</button>
-      <button>Get Profile</button>
-      <button>Logout</button>
+      <button onClick={handleLoginClick}>Login</button>
+      <button onClick={handleGetProfileClick}>Get Profile</button>
+      <button onClick={handleLogoutClick}>Logout</button>
     </div>
   );
 }
